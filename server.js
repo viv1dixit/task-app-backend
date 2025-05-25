def7604d986ae855b58acccb6fdbcb27dad8b415
vivek dixit <vivek542dixit@gmail.com>
2025-05-25 21:12:48 +0530

    port changed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || "https://task-app-backend-pbpv.onrender.com/";
 
 // Middleware
 app.use(cors());
